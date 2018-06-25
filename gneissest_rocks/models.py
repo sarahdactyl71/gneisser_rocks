3d2268ea81710bc956f88c4a9c617f109169918b
Sarah Kirk <kirkse710@gmail.com>
2018-06-25 16:52:15 -0700

    edits models.py for models.Charfield

--- gneissest_rocks/models.py
+++ gneissest_rocks/models.py
@@ -3,5 +3,5 @@ from django.db import models
 class Rocks(models.Model):
     image_url = models.URLField(max_length=200)
     name = models.CharField(max_length=50)
-    description = CharField(max_length=200)
-    location = CharField(max_length=50)
+    description = models.TextField(max_length=200)
+    location = models.CharField(max_length=50)
