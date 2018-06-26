3f12469214224ef4408a4aa9558a89fb4aa65bd3
Sarah Kirk <kirkse710@gmail.com>
2018-06-26 09:17:51 -0700

    adds comment for personal clarity

--- gneissest_rocks/models.py
+++ gneissest_rocks/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
+#this is kind of like formulating a schema that generates the kind of migration
+#file that we are looking for
+
 class Rocks(models.Model):
     image_url = models.URLField(max_length=200)
     name = models.CharField(max_length=50)
