d7e08d1d8f0869f515b4f43712396f706ebef3b6
Sarah Kirk <kirkse710@gmail.com>
2018-06-27 11:10:04 -0700

    added more dummy views to views.py

--- gneissest_rocks/models.py
+++ gneissest_rocks/models.py
@@ -4,6 +4,10 @@ from django.db import models
 #file that we are looking for
 
 class Rock(models.Model):
+
+    def __str__(self):
+        return self.name
+
     image_url = models.URLField(max_length=200)
     name = models.CharField(max_length=50)
     description = models.TextField(max_length=200)
--- gneissest_rocks/views.py
+++ gneissest_rocks/views.py
@@ -2,3 +2,10 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hey, this is a dope app about some lumpin rocks. This is the index!")
+
+def detail(request, rock_id):
+    return HttpResponse("You're looking at rock %s." & rock_id)
+
+def results(request, rock_id):
+    response = "You're looking at the results of rock %s."
+    return HttpResponse(response % question_id)
