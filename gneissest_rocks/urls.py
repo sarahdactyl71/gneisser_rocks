12897b1262cf5de63375af213a6dc97baad7f424
Sarah Kirk <kirkse710@gmail.com>
2018-06-27 11:14:28 -0700

    updates urls.py with newly formulated paths

--- gneissest_rocks/urls.py
+++ gneissest_rocks/urls.py
@@ -3,5 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+#ex /rocks/
     path('', views.index, name='index'),
+    #ex /rocks/5/
+    path('<int:rock_id>/', views.detail, name='detail'),
+    #ex: /rocks/5/results/
+    path('<int:rock_id>/results/', views.results, name='results'),
 ]
